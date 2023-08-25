9dadebb5d027505a820b2d8dc072b499aca5ed40
linoxolus <linoxolus@gmail.com>
2023-08-25 21:15:48 +0700

    Add: lightbox modal

--- src/public/js/media.js
+++ src/public/js/media.js
@@ -1,6 +1,7 @@
 const mediaModal = document.querySelector('.media-modal');
 const mediaItem = document.querySelectorAll('.media-item');
 const closeBtn = document.querySelector('.media-icon.times');
+const mediaImage = document.querySelector('.media-image');
 
 closeBtn.addEventListener('click', (e) => {
     mediaModal.classList.add('closed');
@@ -8,7 +9,12 @@ closeBtn.addEventListener('click', (e) => {
 
 for(i = 0; i < mediaItem.length; i++) {
     mediaItem[i].addEventListener('click', (e) => {
-        var id = e.target.dataset.id;
-        
+        var originImage = e.target.dataset.image;
+        var minImage = e.target.src;
+        mediaImage.src = minImage;
+        mediaImage.onload = (e) => {
+            mediaImage.src = originImage; 
+        }
+        mediaModal.classList.remove('closed');
     })
 }
