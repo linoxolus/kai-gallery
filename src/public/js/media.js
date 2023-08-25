f9cde2d03f0aa1a2eecf371df8d258de209f6595
linoxolus <linoxolus@gmail.com>
2023-08-25 22:15:24 +0700

    Refactor: lightbox

--- src/public/js/media.js
+++ src/public/js/media.js
@@ -1,20 +1,47 @@
 const mediaModal = document.querySelector('.media-modal');
 const mediaItem = document.querySelectorAll('.media-item');
 const closeBtn = document.querySelector('.media-icon.times');
+const overlay = document.querySelector('.overlay');
 const mediaImage = document.querySelector('.media-image');
+const prevBtn = document.querySelector('.media-icon.arrow.prev');
+const nextBtn = document.querySelector('.media-icon.arrow.next');
+var currentIndex = 0;
+
+function openModal(e) {
+    originImage = e.target.dataset.image;
+    minImage = e.target.src;
+    currentIndex = Number(e.target.dataset.id);
+    mediaImage.src = minImage;
+    mediaImage.onload = (e) => {
+        mediaImage.src = originImage;
+    };
+    mediaModal.classList.remove('closed');
+}
+
+function closeModal(e) {
+    mediaModal.classList.add('closed');
+}
+
+function prevImage(e) {
+
+}
+
+function nextImage(e) {
+
+}
+
+for (i = 0; i < mediaItem.length; i++) {
+    mediaItem[i].addEventListener('click', openModal);
+}
 
 closeBtn.addEventListener('click', (e) => {
-    mediaModal.classList.add('closed');
-})
+    closeModal();
+});
 
-for(i = 0; i < mediaItem.length; i++) {
-    mediaItem[i].addEventListener('click', (e) => {
-        var originImage = e.target.dataset.image;
-        var minImage = e.target.src;
-        mediaImage.src = minImage;
-        mediaImage.onload = (e) => {
-            mediaImage.src = originImage; 
-        }
-        mediaModal.classList.remove('closed');
-    })
+overlay.onclick = closeModal;
+
+document.documentElement.onkeyup = (e) => {
+    if(e.keyCode === 27) {
+        closeModal();
+    }
 }
